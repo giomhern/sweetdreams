61cd48e8d8067602e023fdd5428651ef54e49d90
giovannimhern <85322117+giovannimhern@users.noreply.github.com>
2023-10-21 03:12:43 -0500

    Another visual icon sizing change

--- app/components/contact.js
+++ app/components/contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
                 <div className="grid grid-cols-1 gap-12 mt-10 md:grid-cols-2">
                     <div className="flex flex-col items-center justify-center text-center">
                         <span className="p-5 text-sweetblue rounded-full bg-blue-100/80">
-                            <svg className="md:h-[2em] md:w-[2em] h-[1em] w-[1em]" xmlns="http://www.w3.org/2000/svg" fill="none" height="2em"  width="2em" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                            <svg className="md:h-[2em] md:w-[2em] h-[1.5em] w-[1.5em]" xmlns="http://www.w3.org/2000/svg" fill="none" height="2em"  width="2em" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                             </svg>
                         </span>
@@ -21,7 +21,7 @@ const Contact = () => {
                     </div>
                     <div className="flex flex-col items-center justify-center text-center">
                         <span className="p-5 text-sweetblue rounded-full bg-blue-100/80">
-                            <svg className="md:h-[2em] md:w-[2em] h-[1em] w-[1em]" xmlns="http://www.w3.org/2000/svg" fill="none" height="2em"  width="2em" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                            <svg className="md:h-[2em] md:w-[2em] h-[1.5em] w-[1.5em]" xmlns="http://www.w3.org/2000/svg" fill="none" height="2em"  width="2em" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                             </svg>
                         </span>
