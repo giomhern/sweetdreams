1bb2458a7d050ad095e68e7c7117c2a79b01e5d6
giovannimhern <85322117+giovannimhern@users.noreply.github.com>
2023-10-23 00:22:44 -0500

    One final change to wording

--- app/components/offer-services.js
+++ app/components/offer-services.js
@@ -24,7 +24,7 @@ const SpecialServices = () => {
                 <div className='flex-col md:p-10  shadow-lg shadow-blue-100 bg-white p-7'>
                     <div className='flex flex-col items-center justify-center pb-2'>
                        <svg className="md:h-[4em] md:w-[4em] h-[2.5em] w-[2.5em] pb-2" xmlns="http://www.w3.org/2000/svg" fill="none" height="5em" width="5em" viewBox="0 0 24 24"><path d="M15 3V6.4C15 6.96005 15 7.24008 15.109 7.45399C15.2049 7.64215 15.3578 7.79513 15.546 7.89101C15.7599 8 16.0399 8 16.6 8H20M10 8H6C4.89543 8 4 8.89543 4 10V19C4 20.1046 4.89543 21 6 21H12C13.1046 21 14 20.1046 14 19V16M16 3H13.2C12.0799 3 11.5198 3 11.092 3.21799C10.7157 3.40973 10.4097 3.71569 10.218 4.09202C10 4.51984 10 5.0799 10 6.2V12.8C10 13.9201 10 14.4802 10.218 14.908C10.4097 15.2843 10.7157 15.5903 11.092 15.782C11.5198 16 12.0799 16 13.2 16H16.8C17.9201 16 18.4802 16 18.908 15.782C19.2843 15.5903 19.5903 15.2843 19.782 14.908C20 14.4802 20 13.9201 20 12.8V7L16 3Z" stroke="#00BFFF" strokeLinejoin="round"/></svg>
-                        <p className='text-xl text-extrabold text-aliceblue'>Baby Care Tracking Worksheet</p>
+                        <p className='text-xl text-extrabold text-aliceblue'>Baby Tracking Worksheet</p>
                     </div>
                     
                     <p className='text-lg text-cyanblue'> Keep track of your baby’s needs with our intuitive and easy-to-use tracking system.</p>
