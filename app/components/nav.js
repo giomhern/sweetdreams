5475e420fc97530d84c1a09e99c774b6821ba97c
giovannimhern <85322117+giovannimhern@users.noreply.github.com>
2023-10-23 21:07:29 -0500

    Made some changes

--- app/components/nav.js
+++ app/components/nav.js
@@ -8,10 +8,10 @@ const Navigation = () => {
             <nav className="flex justify-between md:py-4 px-5 py-3 md:px-10  w-full">
                 <div className='flex md:space-x-5 space-x-2 items-center justify-center'>
                     <Link href="/"><img className='h-[50px] w-[50px] md:h-[80px] md:w-[80px] ' src='./SWEETDREAMS.png' height="80px" width="80px" alt='this is the logo'></img></Link> 
-                    <Link href="/"><p className="font-thin text-aliceblue md:text-xl  md:visible invisible text-xs hover:text-lightblue">Home</p></Link>
-                    <Link href="#services"><p className="font-thin md:visible invisible text-aliceblue md:text-xl text-xs hover:text-lightblue">Services</p></Link>
-                    <Link href="#about"><p className="font-thin md:visible invisible  text-aliceblue md:text-xl text-xs hover:text-lightblue">About Me</p></Link>
-                    <Link href="#contact"><p className="font-thin md:visible invisible text-aliceblue md:text-xl text-xs hover:text-lightblue">Contact</p></Link>
+                    <Link href="/"><p className="font-thin text-aliceblue md:text-xl  md:visible invisible text-xs hover:text-cyanblue">Home</p></Link>
+                    <Link href="#services"><p className="font-thin md:visible invisible text-aliceblue md:text-xl text-xs hover:text-cyanblue">Services</p></Link>
+                    <Link href="#about"><p className="font-thin md:visible invisible  text-aliceblue md:text-xl text-xs hover:text-cyanblue">About Me</p></Link>
+                    <Link href="#contact"><p className="font-thin md:visible invisible text-aliceblue md:text-xl text-xs hover:text-cyanblue">Contact</p></Link>
                 </div>
                     <div className='flex items-center'>
                         <a href="tel:+19099059441" className='items-center justify-center border-aliceblue text-aliceblue md:border-aliceblue md:text-aliceblue hover:text-lightcyan rounded-full md:border-2 border md:px-5 md:py-2 md:text-xl text-md px-3 py-1 hover:border-aliceblue hover:bg-aliceblue hover:cursor'>
