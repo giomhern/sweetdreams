84f050933fc3c7241dcec7f13bffde9a1cda7b99
giovannimhern <giovannimhern@uchicago.edu>
2024-01-21 23:52:11 -0600

    Fixed whitespace nav issue

--- app/components/nav.js
+++ app/components/nav.js
@@ -1,25 +1,42 @@
 /* eslint-disable @next/next/no-img-element */
-import Link from 'next/link';
-import React, { useState, useRef, useEffect } from 'react';
+import Link from "next/link";
+import React, { useState, useRef, useEffect } from "react";
 
 const Navigation = () => {
-    return (
-        <div>
-            <nav className="flex justify-between md:py-4 px-5 py-3 md:px-10 invisible md:visible  w-full">
-                <div className='flex md:space-x-5 space-x-2 items-center justify-center'>
-                    <Link href="/"><p className="font-thin text-aliceblue md:text-xl  md:visible invisible text-xs hover:text-cyanblue">Home</p></Link>
-                    <Link href="#services"><p className="font-thin md:visible invisible text-aliceblue md:text-xl text-xs hover:text-cyanblue">Services</p></Link>
-                    <Link href="#about"><p className="font-thin md:visible invisible  text-aliceblue md:text-xl text-xs hover:text-cyanblue">About Me</p></Link>
-                    <Link href="#contact"><p className="font-thin md:visible invisible text-aliceblue md:text-xl text-xs hover:text-cyanblue">Contact</p></Link>
-                </div>
-                    <div className='flex items-center'>
-                        <a href="tel:+19099059441" className='items-center justify-center border-aliceblue text-aliceblue md:border-aliceblue md:text-aliceblue hover:text-lightcyan rounded-full md:border-2 border md:px-5 md:py-2 md:text-xl text-md px-3 py-1 hover:border-aliceblue hover:bg-aliceblue hover:cursor'>
-                            Book
-                        </a>
-                    </div>
-            </nav>
-        </div>
-    )
-}
+  return (
+    <nav className="hidden md:flex justify-between py-4 px-5 md:px-10 w-full">
+      <div className="flex space-x-5 items-center justify-center">
+        <Link href="/">
+          <p className="font-thin text-aliceblue text-xl hover:text-cyanblue">
+            Home
+          </p>
+        </Link>
+        <Link href="#services">
+          <p className="font-thin text-aliceblue text-xl hover:text-cyanblue">
+            Services
+          </p>
+        </Link>
+        <Link href="#about">
+          <p className="font-thin text-aliceblue text-xl hover:text-cyanblue">
+            About Me
+          </p>
+        </Link>
+        <Link href="#contact">
+          <p className="font-thin text-aliceblue text-xl hover:text-cyanblue">
+            Contact
+          </p>
+        </Link>
+      </div>
+      <div className="flex items-center">
+        <a
+          href="tel:+19099059441"
+          className="items-center justify-center border-aliceblue text-aliceblue border-aliceblue text-aliceblue hover:text-lightcyan rounded-full border-2 px-5 py-2 text-xl hover:border-aliceblue hover:bg-aliceblue hover:cursor"
+        >
+          Book
+        </a>
+      </div>
+    </nav>
+  );
+};
 
 export default Navigation;
